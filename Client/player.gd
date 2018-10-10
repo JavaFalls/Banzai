c418cab02d18229e0ee185476f68835e92211bb2
Seth <43359955+sethstevens67@users.noreply.github.com>
2018-10-10 16:16:20 -0500

    Added rotation based on curser
    sprite points at curser now

--- Client/player.gd
+++ Client/player.gd
@@ -1,6 +1,9 @@
 extends "res://entity.gd"
+var relative_mouse = Vector2()
 
 func _physics_process(delta):
+	relative_mouse = get_position() - get_viewport().get_mouse_position()
+	
 	direction = Vector2(0,0)
 	if Input.is_action_pressed("ui_right"):
 		direction.x = SPEED
@@ -15,6 +18,20 @@ func _physics_process(delta):
 		direction.y = SPEED
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		direction.y = 0
+		
+#rotate sprite towards the mouse curser
+	if (abs(relative_mouse.x) > abs(relative_mouse.y)):
+		if relative_mouse.x > 0:
+			set_rotation_degrees(90)
+		else:
+			set_rotation_degrees(270)
+	else:
+		if relative_mouse.y > 0:
+			set_rotation_degrees(180)
+		else:
+			set_rotation_degrees(0)
+
 
 	direction = move_and_slide(direction, UP)
-	pass
+
+	
