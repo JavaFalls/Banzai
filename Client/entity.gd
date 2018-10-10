43e6080f7a7e7618b376e204d1694a5232f94c7c
Seth <43359955+sethstevens67@users.noreply.github.com>
2018-10-10 16:17:55 -0500

    nothing

--- Client/entity.gd
+++ Client/entity.gd
@@ -3,9 +3,12 @@ extends KinematicBody2D
 const UP = Vector2(0,0)
 const SPEED =  300
 var direction = Vector2()
+var this_node = Node2D
+var ev = InputEventAction.new()
+
 
 #func _ready():
-	#player_node = get_node("player")
+#	this_node = get_node("entity")
 
 
 #func _physics_process(delta):
