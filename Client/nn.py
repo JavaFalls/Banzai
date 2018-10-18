e93ae612fbd7ac95058e793542a8846d0146711b
bobjonvon <vaughanjonathan1@gmail.com>
2018-10-17 22:05:17 -0500

    nn

--- Client/nn.py
+++ Client/nn.py
@@ -1,10 +1,17 @@
 import sys
-
-
+import tensorflow as tf
+import keras
+def build_model():
+   model = keras.Sequential([keras.layers.Dense(1, activation=tf.nn.relu, input_shape=([1,1],[1,1],1,1,)),
+   keras.layers.Dense(6, activation=tf.nn.relu),
+   keras.layers.Dense(6, activation=tf.nn.relu),
+   keras.layers.Dense(1)
+   ])
 
 def main():
    
     print(sys.argv)
+    bot = build_model()
     return()
 
 main()
