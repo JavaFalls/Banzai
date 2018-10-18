f542764c3cf0c0ed2f1cd546a8695dc2d0cc9590
Seth <43359955+sethstevens67@users.noreply.github.com>
2018-10-17 22:13:40 -0500

    bug fixes
    there were some things comming up in the debbuger about the bot not having a projectile container. also some comments in entity.

--- Client/arena_train.gd
+++ Client/arena_train.gd
@@ -8,14 +8,15 @@ var start_pos1 = Vector2(320,320)
 var start_pos2 = Vector2(320,160)
 
 func _ready():
-	#fighter1 = player_scene.instance()
-	#self.add_child(fighter1)
-	#fighter1.set_position(start_pos1)
+	#add code here to choose who fights (player or AIs)
+	fighter1 = player_scene.instance()
+	self.add_child(fighter1)
+	fighter1.set_position(start_pos1)
 	
-	#fighter2 = bot_scene.instance()
-	#self.add_child(fighter2)
-	#fighter2.set_position(start_pos2)
-	pass
+	fighter2 = bot_scene.instance()
+	self.add_child(fighter2)
+	fighter2.set_position(start_pos2)
+#	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
--- Client/entity.gd
+++ Client/entity.gd
@@ -1,18 +1,18 @@
 extends KinematicBody2D
+ 
+const UP             = Vector2(0,0) # Indicates top-down view
+const MOVEMENT_SPEED =  300         # Movement speed of the entity
 
-const UP = Vector2(0,0)
-const MOVEMENT_SPEED =  300
-var direction = Vector2()
-onready var player_node = get_parent().get_node("player")
-var this_node = Node2D
-var p = Area2D
-var ev = InputEventAction.new()
-# or #export (PackedScene) var projectile
-onready var projectile_container = get_node("player_projectiles")
-onready var projectile = preload("res://projectile.tscn")
-var timer = Timer.new()
-var projectile_delay = .3
-var hit_points = 0
+var p                = Area2D      # New Projectile
+var timer            = Timer.new() # Timer for Firing cooldown
+var projectile_delay = .3          # Firing cooldown length
+var hit_points       = 0           # How many times the entity has been hit
+var direction        = Vector2()   # Direction the entity is facing
+
+onready var projectile_container = get_node("projectile_container")   # Where the projectiles are stored
+onready var projectile           = preload("res://projectile.tscn") # The projectile scene to be instanced
+onready var player_node          = get_parent().get_node("player")  # A reference to the 
+
 #onready var ZZ = get_node("ZZ")
 
 func shoot():
@@ -26,7 +26,7 @@ func shoot():
 	p.shoot_at_mouse(self.global_position)
 	
 
-#func _ready():
+func _ready():
 	timer.set_wait_time(projectile_delay)
 	timer.set_one_shot(true)
 	self.add_child(timer)
@@ -43,3 +43,8 @@ func restart_timer(delay):
 
 func get_hit_points():
 	return hit_points
+	
+func choose_weapon(weapon_choice):
+	# GDScript does support case statements
+	if weapon_choice is 1:
+		pass
