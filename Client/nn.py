eaa67153d9959f52837e63d77fc8922fccf266d3
bobjonvon <vaughanjonathan1@gmail.com>
2018-10-19 15:35:09 -0500

    Neural Network
    properly imported keras

--- Client/nn.py
+++ Client/nn.py
@@ -1,17 +1,23 @@
 import sys
 import tensorflow as tf
-import keras
+from tensorflow import keras
+import numpy as np
+import copy
 def build_model():
-   model = keras.Sequential([keras.layers.Dense(1, activation=tf.nn.relu, input_shape=([1,1],[1,1],1,1,)),
+   model = keras.Sequential([keras.layers.Dense(6, activation=tf.nn.relu, input_shape=(1,1,1,1,1,1,)),
    keras.layers.Dense(6, activation=tf.nn.relu),
    keras.layers.Dense(6, activation=tf.nn.relu),
    keras.layers.Dense(1)
    ])
 
 def main():
-   
-    print(sys.argv)
+    
+    number_of_factors = 0
+    for line in sys.argv:
+        print(line)
+        number_of_factors=+1
     bot = build_model()
+    print(number_of_factors)
     return()
 
 main()
