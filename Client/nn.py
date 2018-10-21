c59ae4956c0544625a17907c456daee30752a104
bobjonvon <vaughanjonathan1@gmail.com>
2018-10-20 21:01:15 -0500

    Added save and load functionality to the neural network
    The neural network can now be saved and loaded.

--- Client/nn.py
+++ Client/nn.py
@@ -1,7 +1,15 @@
 import sys
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras.models import load_model
 
+
+def save_bot(model):
+   model.save('my_model1.h5')
+
+def load_bot():
+   model = load_model('my_model1.h5')
+   return model
 
 def build_model():
    model = keras.Sequential([keras.layers.Dense(6, activation=tf.nn.relu, input_shape=(1,1,1,1,1,1,)),
@@ -9,14 +17,23 @@ def build_model():
    keras.layers.Dense(6, activation=tf.nn.relu),
    keras.layers.Dense(1)])
 
+   
+   model.compile(loss='mse',
+                 optimizer="Adam",
+                 metrics=['mae'])
+      
+   return model
+
 def main():
     
-    number_of_factors = 0
-    for line in sys.argv:
-        print(line)
-        number_of_factors=+1
+    #number_of_factors = 0
+    #for line in sys.argv:
+     #   print(line)
+      #  number_of_factors=+1
+    bot = load_bot()
     bot = build_model()
-    print(number_of_factors)
+    save_bot(bot)
+    #print(number_of_factors)
     return()
 
 main()
