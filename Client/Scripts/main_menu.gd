84a70713c1021bf2fe2ae5629835697946b91c52
Alex Zacharias <43507078+Zinjanjie@users.noreply.github.com>
2018-10-24 20:36:29 -0500

    Resize backgrounds with viewport

--- Client/Scripts/main_menu.gd
+++ Client/Scripts/main_menu.gd
@@ -1,6 +1,12 @@
 extends Node
 
+onready var _macros = preload("res://Scripts/macros.gd")
+onready var _background = get_node("MarginContainer2/background")
+
 func _ready():
+	get_tree().get_root().connect("size_changed", self, "_resize")
+	var size = OS.get_window_size()
+	_background.scale = Vector2(size.x / _macros.NORMAL_WIDTH, size.y / _macros.NORMAL_HEIGHT)
 	pass
 
 func _process(delta):
@@ -14,4 +20,9 @@ func _on_train_button_pressed():
 
 func _on_fight_button_pressed():
 	get_tree().change_scene("res://Scenes/arena_battle.tscn")
+	pass
+
+func _resize():
+	var size = OS.get_window_size()
+	_background.scale = Vector2(size.x / _macros.NORMAL_WIDTH, size.y / _macros.NORMAL_HEIGHT)
 	pass
--- Client/Scripts/menu_title.gd
+++ Client/Scripts/menu_title.gd
@@ -1,8 +1,12 @@
 extends Node
 
-
+onready var _macros = preload("res://Scripts/macros.gd")
+onready var _background = get_node("MarginContainer/background")
 
 func _ready():
+	get_tree().get_root().connect("size_changed", self, "_resize")
+	var size = OS.get_window_size()
+	_background.scale = Vector2(size.x / _macros.NORMAL_WIDTH, size.y / _macros.NORMAL_HEIGHT)
 	pass
 	
 func _process(delta):
@@ -12,5 +16,7 @@ func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		get_tree().change_scene("res://Scenes/main_menu.tscn")
 
-#func _process(delta):
-#	pass
+func _resize():
+	var size = OS.get_window_size()
+	_background.scale = Vector2(size.x / _macros.NORMAL_WIDTH, size.y / _macros.NORMAL_HEIGHT)
+	pass
