f96f272c917e41b55bcfb0981db1a913065db1c8
Alex Zacharias <43507078+Zinjanjie@users.noreply.github.com>
2018-10-26 20:13:00 -0500

    Start timer on inactive mouse

--- Client/Scripts/main_menu.gd
+++ Client/Scripts/main_menu.gd
@@ -3,19 +3,21 @@ extends Node
 onready var _macros = preload("res://Scripts/macros.gd")
 onready var _background = get_node("MarginContainer/background")
 onready var _timer = get_node("timeout")
-var idle
 
 func _ready():
 	get_tree().get_root().connect("size_changed", self, "_resize")
 	var size = OS.get_window_size()
 	_background.scale = Vector2(size.x / _macros.NORMAL_WIDTH, size.y / _macros.NORMAL_HEIGHT)
-	
-	_timer.start()
 	pass
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().change_scene("res://Scenes/menu_title.tscn")
+	pass
+
+func _input(event):
+	if event is InputEventMouse:
+		_timer.start()
 	pass
 
 func _on_train_button_pressed():
