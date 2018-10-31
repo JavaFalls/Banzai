9135a5c7dd4259d5d7bfce5c36c0027477a7c22f
Alex Zacharias <43507078+Zinjanjie@users.noreply.github.com>
2018-10-30 23:42:06 -0500

    Create username interface

--- Client/Scripts/menu_title.gd
+++ Client/Scripts/menu_title.gd
@@ -1,6 +1,13 @@
 extends Node
 
 onready var _macros = preload("res://Scripts/macros.gd")
+
+onready var _manufacturor = get_node("VBoxContainer/HBoxContainer/manufacturor")
+onready var _name = get_node("VBoxContainer/HBoxContainer/name")
+onready var _code = get_node("VBoxContainer/HBoxContainer/code")
+
+onready var _slider = get_node("VBoxContainer/HSlider")
+
 onready var _background = get_node("MarginContainer/background")
 
 func _ready():
@@ -10,11 +17,15 @@ func _ready():
 	pass
 	
 func _process(delta):
+#	get_tree().change_scene("res://Scenes/main_menu.tscn")
 	pass
 	
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
-		get_tree().change_scene("res://Scenes/main_menu.tscn")
+		_slider.show()
+		_manufacturor.popup()
+		_name.popup()
+		_code.popup()
 
 func _resize():
 	var size = OS.get_window_size()
