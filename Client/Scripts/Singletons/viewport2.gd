4856e844cd95f7e1d49b0ef1c7051630bdcf755b
Alex Zacharias <43507078+Zinjanjie@users.noreply.github.com>
2018-11-14 11:46:15 -0600

    Change render settings

--- Client/Scripts/Singletons/viewport2.gd
+++ Client/Scripts/Singletons/viewport2.gd
@@ -1,4 +1,3 @@
-# Script from chanon and sysharm
 # https://github.com/godotengine/godot/issues/6506
 
 extends Node
@@ -7,19 +6,23 @@ extends Node
 onready var viewport = get_viewport()
 
 func _ready():
-    get_tree().connect("screen_resized", self, "_screen_resized")
+	get_tree().connect("screen_resized", self, "_screen_resized")
 
 func _screen_resized():
-    var window_size = OS.get_window_size()
-
-    var scale_x = window_size.x / viewport.size.x
-    var scale_y = window_size.y / viewport.size.y
-
-    var scale = min(scale_x, scale_y)
-
-    # find the coordinate we will use to center the viewport inside the window
-    var diff = window_size - (viewport.size * scale)
-    var diffhalf = (diff * 0.5)
-
-    # attach the viewport to the rect we calculated
-    viewport.set_attach_to_screen_rect(Rect2(diffhalf, viewport.size * scale))
+	var window_size = OS.get_window_size()
+	
+	var scale_x = window_size.x / viewport.size.x
+	var scale_y = window_size.y / viewport.size.y
+	
+	var scale = min(scale_x, scale_y)
+	
+	# find the coordinate we will use to center the viewport inside the window
+	var diff = window_size - (viewport.size * scale)
+	var diffhalf = (diff * 0.5)
+	
+	# attach the viewport to the rect we calculated
+	viewport.set_attach_to_screen_rect(Rect2(diffhalf, viewport.size * scale))
+	
+	# draw black borders
+	
+	
