bb604454a41e3e716ca392505f56fc15d3a05c7d
bobjonvon <vaughanjonathan1@gmail.com>
2019-01-28 21:01:30 -0600

    added a game state object.
    The new game state object makes interacting with the neural network easier by packaging and unpacking neural network parameters and outputs.

---
+++ Client/game_state.gd
@@ -0,0 +1,26 @@
+extends Node
+onready var player_x = 0
+onready var player_y = 0
+onready var player_mouse_x = 0
+onready var player_mouse_y = 0
+onready var player_vector_x = 0
+onready var player_vector_y = 0
+onready var player_w        = 0
+onready var player_a        = 0
+onready var player_s        = 0
+onready var player_d        = 0
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
