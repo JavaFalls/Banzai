e561fb643833330048db60e3e5310e68f3aa00d9
bobjonvon <vaughanjonathan1@gmail.com>
2019-02-01 14:10:46 -0600

    continued work on game_state object

--- Client/game_state.gd
+++ Client/game_state.gd
@@ -5,10 +5,7 @@ onready var player_mouse_x = 02
 onready var player_mouse_y = 0
 onready var player_vector_x = 0
 onready var player_vector_y = 03
-onready var player_w        = 0
-onready var player_a        = 04
-onready var player_s        = 0
-onready var player_d        = 0
+
 
 onready var bot_x = 0
 onready var bot_y = 05
@@ -16,10 +13,22 @@ onready var bot_mouse_x = 0
 onready var bot_mouse_y = 0
 onready var bot_vector_x = 06
 onready var bot_vector_y = 0
-onready var bot_w        = 0
-onready var bot_a        = 07
-onready var bot_s        = 0
-onready var bot_d        = 0
+
+onready var predicted_player_x = 01
+onready var predicted_player_y = 0
+onready var predicted_player_mouse_x = 02
+onready var predicted_player_mouse_y = 0
+onready var predicted_player_vector_x = 0
+onready var predicted_player_vector_y = 03
+
+
+onready var predicted_bot_x = 0
+onready var predicted_bot_y = 05
+onready var predicted_bot_mouse_x = 0
+onready var predicted_bot_mouse_y = 0
+onready var predicted_bot_vector_x = 06
+onready var predicted_bot_vector_y = 0
+
 
 # class member variables go here, for example:
 # var a = 2
@@ -32,10 +41,7 @@ func get_battle_state():
 	game_state.append(self.bot_mouse_y)
 	game_state.append(self.bot_vector_x)
 	game_state.append(self.bot_vector_y)
-	game_state.append(self.bot_w)
-	game_state.append(self.bot_a)
-	game_state.append(self.bot_s)
-	game_state.append(self.bot_d)
+
 	
 	
 	game_state.append(self.player_x)
@@ -44,10 +50,7 @@ func get_battle_state():
 	game_state.append(self.player_mouse_y)
 	game_state.append(self.player_vector_x)
 	game_state.append(self.player_vector_y)
-	game_state.append(self.player_w)
-	game_state.append(self.player_a)
-	game_state.append(self.player_s)
-	game_state.append(self.player_d)
+
 	
 	return game_state
 	
@@ -59,10 +62,7 @@ func get_training_state():
 	game_state.append(self.player_mouse_y)
 	game_state.append(self.player_vector_x)
 	game_state.append(self.player_vector_y)
-	game_state.append(self.player_w)
-	game_state.append(self.player_a)
-	game_state.append(self.player_s)
-	game_state.append(self.player_d)
+
 	
 	game_state.append(self.bot_x)
 	game_state.append(self.bot_y)
@@ -70,10 +70,7 @@ func get_training_state():
 	game_state.append(self.bot_mouse_y)
 	game_state.append(self.bot_vector_x)
 	game_state.append(self.bot_vector_y)
-	game_state.append(self.bot_w)
-	game_state.append(self.bot_a)
-	game_state.append(self.bot_s)
-	game_state.append(self.bot_d)
+
 	return game_state
 	
 func set_bot_state(bot):
@@ -81,11 +78,7 @@ func set_bot_state(bot):
 	self.bot_y        = bot.get_y()
 	self.bot_mouse_x  = bot.get__mouse_x()
 	self.bot_vector_x = bot.get_vector_x()
-	self.bot_vector_y = bot.get_vector_y()
-	self.bot_w        = bot.get_w()
-	self.bot_a        = bot.get_a()
-	self.bot_s        = bot.get_s()
-	self.bot_d        = bot.get_d()
+
 	
 func set_player_state(player):
 	self.player_x        = player.get_x()
@@ -94,10 +87,23 @@ func set_player_state(player):
 	self.player_mouse_y  = player.get_mouse_y()
 	self.player_vector_x = player.get_vector_x()
 	self.player_vector_y = player.get_vector_y()
-	self.player_w        = player.get_w()
-	self.player_a        = player.get_a()
-	self.player_s        = player.get_s()
-	self.player_d        = player.get_d()
+
+func set_predictions(predictions):
+	predicted_player_x = 01
+	predicted_player_y = 0
+	predicted_player_mouse_x = 02
+	predicted_player_mouse_y = 0
+	predicted_player_vector_x = 0
+	predicted_player_vector_y = 03
+
+
+	predicted_bot_x = 0
+	predicted_bot_y = 05
+	predicted_bot_mouse_x = 0
+	predicted_bot_mouse_y = 0
+	predicted_bot_vector_x = 06
+	predicted_bot_vector_y = 0
+
 	
 func _ready():
 	# Called when the node is added to the scene for the first time.
