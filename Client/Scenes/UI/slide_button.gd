a9b3c3a9a0d19a08721173ac6a8beec181a5d284
Alex Zacharias <43507078+Zinjanjie@users.noreply.github.com>
2019-02-10 23:25:36 -0600

    Limited the number of chars per name

--- Client/Scenes/UI/slide_button.gd
+++ Client/Scenes/UI/slide_button.gd
@@ -1,13 +1,12 @@
 extends Path2D
 
 export(String) var text = "text"
-export(int, -1, 1000) var length = -1
+export(int, -1, 100) var max_chars = -1
+export(int, -1, 1000) var box_width = -1
 export(float, 0.0, 1000.0) var duration = 0.0
 export(float, 0.0, 1000.0) var delay = 0.0
 export(int, 0, 10) onready var transition_type = Tween.TRANS_CUBIC
 export(int, 0, 3) onready var ease_type = Tween.EASE_OUT
-
-export(Theme) var theme
 
 onready var texture_node = get_node("PathFollow2D/Button/NinePatchRect")
 onready var idle_texture = preload("res://assets/menu/name_choice/button_long.png")
@@ -15,11 +14,11 @@ onready var hover_texture = preload("res://assets/menu/name_choice/button_long_s
 
 func _ready():
 	var button = get_node("PathFollow2D/Button")
+	if max_chars != -1:
+		text = text.left(max_chars)
 	button.text = text
-	if not length < 0:
-		button.rect_size.x = length
-	if typeof(theme) != TYPE_NIL:
-		button.theme = theme
+	if box_width != -1:
+		button.rect_size.x = box_width
 
 func slide():
 	var tween = Tween.new()
