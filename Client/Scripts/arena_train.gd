01a7879e7048f788bb60c9998edafdb4347fe190
Wargath <adam.baldwin@students.pcci.edu>
2019-02-23 07:50:09 -0600

    Added a popup to arena train.

--- Client/Scripts/arena_train.gd
+++ Client/Scripts/arena_train.gd
@@ -1,19 +1,22 @@
 extends Node2D
 
 # The Bot
-var Bot = JSON.parse(head.DB.get_bot(head.bot_ID)).result["data"][0]
+onready var Bot = JSON.parse(head.DB.get_bot(head.bot_ID)).result["data"][0]
 
 # The variables
 var fighter1                             # Player or his AI bot
 var fighter2                             # Opponent of player or AI bot
-var start_pos1 = Vector2(200,48)         # Where the first fighter spawns
+var start_pos1 = Vector2(200,73)         # Where the first fighter spawns
 var start_pos2 = Vector2(200,175)        # Where the second fighter spawns
 
+var popup                                # Popup scene used when battle is over
+
 # The preloaded scenes
-onready var player_scene = preload("res://Scenes/player.tscn")
-onready var bot_scene    = preload("res://Scenes/bot.tscn")
-onready var dummy_scene  = preload("res://Scenes/dummy.tscn")
-onready var game_state   = get_node("game_state")
+onready var player_scene    = preload("res://Scenes/player.tscn")
+onready var bot_scene       = preload("res://Scenes/bot.tscn")
+onready var dummy_scene     = preload("res://Scenes/dummy.tscn")
+onready var arena_end_popup = preload("res://Scenes/popups/arena_end_popup.tscn")
+onready var game_state      = get_node("game_state")
 
 func _ready():
 #	 The player's bot or AI
@@ -37,7 +40,9 @@ func _ready():
 
 # Called after game ends
 func game_end():
-	head.DB.update_bot(Bot["Bot_ID"])
+	popup = arena_end_popup.instance()
+	self.add_child(popup)
+	popup.init("Training Has Ended", "Yes", "No", self, "keep_data", self, "drop_data", "Would you like your robot to learn from this session?")
 
 
 func _process(delta):
@@ -46,7 +51,7 @@ func _process(delta):
 
 func send_nn_state():
 	var output = []
-	var path = PoolStringArray() 
+	var path = PoolStringArray()
 	var predictions = []
 #	path.append('C:/Users/vaugh/Desktop/wonderwoman/Banzai/Client/NeuralNetwork/client.py')
 	path.append('D:/Program Files/GitHub/Banzai/Client/NeuralNetwork/client.py')
@@ -63,3 +68,24 @@ func send_nn_state():
 		predictions.append(x)
 #	print(predictions)
 	return predictions
+
+# Popup Functions
+func keep_data():
+	get_tree().paused = false
+	#head.DB.update_model(head.model_ID, "idk_what_the_filename_for_the_model_would_be")
+	popup.free()
+	final_popup()
+func drop_data():
+	get_tree().paused = false
+	popup.free()
+	final_popup()
+func final_popup():
+	popup = arena_end_popup.instance()
+	self.add_child(popup)
+	popup.init("Training Has Ended", "Contiune", "Main Menu", self, "contiune_training", self, "main_menu", "Keep training or return to the main menu?")
+func contiune_training():
+	get_tree().paused = false
+	get_tree().change_scene("res://Scenes/Load_training.tscn")
+func main_menu():
+	get_tree().paused = false
+	get_tree().change_scene("res://Scenes/main_menu.tscn")
