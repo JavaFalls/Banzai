99ac0cfb45e098d2763492485bfa2ae5961e764a
Seth <43359955+sethstevens67@users.noreply.github.com>
2019-03-04 19:22:04 -0600

    encode player actions
    player actions become a 0-107 number that is the same as the nn's output.

--- Client/Scripts/player.gd
+++ Client/Scripts/player.gd
@@ -6,30 +6,29 @@ onready var game_state = self.get_parent().get_child(1) #Currently child 1 is ga
 var relative_mouse = Vector2()
 
 func _process(delta):
+	game_state.set_player_action(self)
 	if is_player:
 		game_state.set_opponent_state(self)
 	else:
 		game_state.set_bot_state(self)
-#	print("set_player_state")
 
 func _physics_process(delta):
 	psuedo_mouse     = get_global_mouse_position()
 	relative_mouse   = get_viewport().get_mouse_position() - get_position()
-	aim_angle        = atan2(relative_mouse.x, relative_mouse.y) # gives angle in radians
 	direction        = Vector2(0,0)
 	psuedo_ability   = 0
 	psuedo_secondary = 0
 	psuedo_primary   = 0
 	
-	if Input.is_action_pressed("primary_attack"):
-		primary_weapon.use() 
-		psuedo_primary = 1
-	if Input.is_action_pressed("secondary_attack"):
-		secondary_weapon.use()
-		psuedo_secondary = 1
 	if Input.is_action_pressed("ability"):
 		ability.use()
 		psuedo_ability = 1
+	elif Input.is_action_pressed("secondary_attack"):
+		secondary_weapon.use()
+		psuedo_secondary = 1
+	elif Input.is_action_pressed("primary_attack"):
+		primary_weapon.use() 
+		psuedo_primary = 1
 	if Input.is_action_pressed("ui_right"):
 		direction.x = 1
 	if Input.is_action_pressed("ui_left"):
@@ -42,6 +41,20 @@ func _physics_process(delta):
 		direction.y = 1
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		direction.y = 0
+		
+		
+	aim_angle_old    = aim_angle
+	aim_angle        = atan2(relative_mouse.x, relative_mouse.y) # gives angle in radians
+	aim_angle_diff = aim_angle - aim_angle_old
+	if abs(aim_angle_diff) > .5:
+		aim_angle_diff *= -1
+	if aim_angle_diff > 0:
+		psuedo_aim_right = 1
+	elif aim_angle_diff < 0:
+		psuedo_aim_left  = 1
+	else:
+		psuedo_aim_right = 0
+		psuedo_aim_left  = 0
 		
 	# Control bot animation
 	if (psuedo_mouse.x > get_position().x):
