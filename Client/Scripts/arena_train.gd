cb6694ffef1a455060a17195ab91b5eff85d7512
Alex Zacharias <43507078+Zinjanjie@users.noreply.github.com>
2019-03-06 21:16:37 -0600

    Add exit popup on training
    scene -> arena_train

--- Client/Scripts/arena_train.gd
+++ Client/Scripts/arena_train.gd
@@ -23,6 +23,7 @@ func _ready():
 #	 The player's bot or AI
 	fighter1 = player_scene.instance()
 	self.add_child(fighter1)
+	fighter1.set_pause_mode(Node.PAUSE_MODE_STOP)
 	fighter1.set_position(start_pos1)
 	fighter1.set_name("fighter1")
 	fighter1.is_player = 1
@@ -31,6 +32,7 @@ func _ready():
 
 	fighter2 = bot_scene.instance()
 	self.add_child(fighter2)
+	fighter2.set_pause_mode(Node.PAUSE_MODE_STOP)
 	fighter2.set_position(start_pos2)
 	fighter2.set_name("fighter2")
 	fighter2.get_node("animation_bot").load_colors_from_DB(head.bot_ID)
@@ -42,6 +44,7 @@ func _ready():
 	t.set_wait_time(.3)
 	t.set_one_shot(true)
 	self.add_child(t)
+	t.set_pause_mode(Node.PAUSE_MODE_STOP)
 	t.start()
 
 # Called after game ends
@@ -55,6 +58,12 @@ func _process(delta):
 	if t.is_stopped():
 		send_nn_state()
 		t.start()
+
+func _input(event):
+	if Input.is_action_just_pressed("exit_arena"):
+		if not get_tree().is_paused():
+			get_tree().set_pause(true)
+			$exit.visible = true
 
 func send_nn_state():
 	var output = []
@@ -86,3 +95,11 @@ func contiune_training():
 func main_menu():
 	get_tree().paused = false
 	get_tree().change_scene("res://Scenes/main_menu.tscn")
+
+func _on_confirm_pressed():
+	get_tree().set_pause(false)
+	get_tree().change_scene("res://Scenes/main_menu.tscn")
+
+func _on_back_pressed():
+	get_tree().set_pause(false)
+	$exit.visible = false
