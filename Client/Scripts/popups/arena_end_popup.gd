35a830b2f8d9b8d66099bdee23485e182eadac7b
Alex Zacharias <43507078+Zinjanjie@users.noreply.github.com>
2019-03-27 21:47:53 -0500

    Change button hover text colors

--- Client/Scripts/popups/arena_end_popup.gd
+++ Client/Scripts/popups/arena_end_popup.gd
@@ -13,9 +13,10 @@ const NP_OPTIONAL_MESSAGE_TEXT = "optional_message_container/optional_message"
 # Godot Hooks:
 #-------------------------------------------------------------------------------
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	$left_button.connect("mouse_entered", self, "button_mouse_entered", [$left_button/Label])
+	$right_button.connect("mouse_entered", self, "button_mouse_entered", [$right_button/Label])
+	$left_button.connect("mouse_exited", self, "button_mouse_exited", [$left_button/Label])
+	$right_button.connect("mouse_exited", self, "button_mouse_exited", [$right_button/Label])
 
 # Functions:
 #-------------------------------------------------------------------------------
@@ -37,3 +38,9 @@ func init(title_text,
 
 func set_background_transparency(value):
 	get_node("shade").color = Color(0,0,0,value)
+
+func button_mouse_entered(label):
+	label.modulate = Color("#53545a")
+
+func button_mouse_exited(label):
+	label.modulate = Color("#2b2c32")
