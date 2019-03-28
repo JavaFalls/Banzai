8e43aca0a62b6b854aaff6474690da30c9672f66
Alex Zacharias <43507078+Zinjanjie@users.noreply.github.com>
2019-03-27 23:23:08 -0500

    I may have overdone it

--- Client/Scripts/main_menu.gd
+++ Client/Scripts/main_menu.gd
@@ -9,6 +9,22 @@ onready var _tween = get_node("Tween")
 onready var _bot = $animation_bot
 
 var ty34918jj = false
+var prevent_messengers_to_zorro = { # Track if messenger is dead. If so, keep sending messengers.
+	'messenger_ferdinand': true,
+	'messenger_dutch': true,
+	'messenger_gibson': true,
+	'messenger_Stan_Lee': true,
+	'messenger_carl': true
+}
+func reincarnate():
+	for dude in prevent_messengers_to_zorro:
+		prevent_messengers_to_zorro[dude] = true
+var HELP = "well?"
+var phase1 = "world in peril"
+var phase2 = "hope in the air"
+var phase3 = "zorro hears the whispers"
+var phase4 = "swing into action"
+var phase4point5 = "horse runs into a wall"
 
 func _ready():
 	if(!Menu_audio.menu_audio.playing):
@@ -34,28 +50,34 @@ func _input(event):
 			_bot.start_walking_forward()
 		else:
 			_bot.start_walking_backward()
+	if event is InputEventKey:
+		if event.scancode == KEY_G:
+			prevent_messengers_to_zorro["messenger_ferdinand"] = false
+			HELP = phase1
+			print(HELP)
+		elif event.scancode == KEY_E and not prevent_messengers_to_zorro["messenger_ferdinand"]:
+			prevent_messengers_to_zorro["messenger_dutch"] = false
+			HELP = phase2
+			print(HELP)
+		elif event.scancode == KEY_A and not prevent_messengers_to_zorro["messenger_dutch"]:
+			prevent_messengers_to_zorro["messenger_gibson"] = false
+			HELP = phase3
+			print(HELP)
+		elif event.scancode == KEY_R and not prevent_messengers_to_zorro["messenger_gibson"]:
+			prevent_messengers_to_zorro["messenger_Stan_Lee"] = false
+			HELP = phase4
+			print(HELP)
+		elif event.scancode == KEY_Y and not prevent_messengers_to_zorro["messenger_Stan_Lee"]:
+			prevent_messengers_to_zorro["messenger_carl"] = false
+			HELP = phase4point5
+			print(HELP) # I keep printing help
+		elif event.scancode == KEY_ENTER and not prevent_messengers_to_zorro["messenger_carl"]:
+			zorro_is_coming()
+		else:
+			HELP = "not today..."
+			reincarnate()
 
 func _process(delta):
-	if Input.is_key_pressed(KEY_G) and Input.is_key_pressed(KEY_E) and Input.is_key_pressed(KEY_A) and Input.is_key_pressed(KEY_R) and Input.is_key_pressed(KEY_Y):
-		ty34918jj = true
-		weapon_creator.get_weapon_stats(weapon_creator.W_PRI_ZORROS_GLARE)["implemented"] = true
-		weapon_creator.get_weapon_stats(weapon_creator.W_SEC_ZORROS_WIT)["implemented"] = true
-		weapon_creator.get_weapon_stats(weapon_creator.W_ABI_ZORROS_HONOR)["implemented"] = true
-		_bot.set_bot_type(_bot.ANIMATION_SET_B1_ZORRO)
-		head.DB.update_bot(head.bot_ID,
-		                   [
-						    head.DB.NULL_INT, # UPDATE_BOT_ARGS_PLAYER_ID
-							head.DB.NULL_INT, # UPDATE_BOT_ARGS_MODEL_ID
-							head.DB.NULL_INT, # UPDATE_BOT_ARGS_RANKING
-							head.DB.NULL_INT, # UPDATE_BOT_ARGS_PRIMARY_WEAPON
-							head.DB.NULL_INT, # UPDATE_BOT_ARGS_SECONDARY_WEAPON
-							head.DB.NULL_INT, # UPDATE_BOT_ARGS_UTILITY
-							head.DB.NULL_COLOR, # UPDATE_BOT_ARGS_PRIMARY_COLOR
-							head.DB.NULL_COLOR, # UPDATE_BOT_ARGS_SECONDARY_COLOR
-							head.DB.NULL_COLOR, # UPDATE_BOT_ARGS_ACCENT_COLOR
-							_bot.ANIMATION_SET_B1_ZORRO # UPDATE_BOT_ARGS_ANIMATION
-						   ],
-						   "")
 	if ty34918jj:
 		var look_at = get_tree().get_root().get_mouse_position() - _bot.position
 		if look_at.x != 0 and look_at.y != 0:
@@ -91,6 +113,26 @@ func button_hover_enter():
 
 func screen_idle_timeout():
 	get_node("logout_warning").hide()
+func zorro_is_coming():
+	ty34918jj = true
+	weapon_creator.get_weapon_stats(weapon_creator.W_PRI_ZORROS_GLARE)["implemented"] = true
+	weapon_creator.get_weapon_stats(weapon_creator.W_SEC_ZORROS_WIT)["implemented"] = true
+	weapon_creator.get_weapon_stats(weapon_creator.W_ABI_ZORROS_HONOR)["implemented"] = true
+	_bot.set_bot_type(_bot.ANIMATION_SET_B1_ZORRO)
+	head.DB.update_bot(head.bot_ID,
+	                   [
+					    head.DB.NULL_INT, # UPDATE_BOT_ARGS_PLAYER_ID
+						head.DB.NULL_INT, # UPDATE_BOT_ARGS_MODEL_ID
+						head.DB.NULL_INT, # UPDATE_BOT_ARGS_RANKING
+						head.DB.NULL_INT, # UPDATE_BOT_ARGS_PRIMARY_WEAPON
+						head.DB.NULL_INT, # UPDATE_BOT_ARGS_SECONDARY_WEAPON
+						head.DB.NULL_INT, # UPDATE_BOT_ARGS_UTILITY
+						head.DB.NULL_COLOR, # UPDATE_BOT_ARGS_PRIMARY_COLOR
+						head.DB.NULL_COLOR, # UPDATE_BOT_ARGS_SECONDARY_COLOR
+						head.DB.NULL_COLOR, # UPDATE_BOT_ARGS_ACCENT_COLOR
+						_bot.ANIMATION_SET_B1_ZORRO # UPDATE_BOT_ARGS_ANIMATION
+					   ],
+					   "")
 
 func logout():
 	fade()
