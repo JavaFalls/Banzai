501db3e2d0ca3621d65cb10e94ac1a02e6f80c7c
Alex Zacharias <43507078+Zinjanjie@users.noreply.github.com>
2019-04-05 23:04:07 -0500

    Included option to show silder values as percentages

--- Client/Scripts/ui_objects/slide_option.gd
+++ Client/Scripts/ui_objects/slide_option.gd
@@ -10,6 +10,7 @@ export(int) var min_value = 0
 export(int) var max_value = 10
 export(int, 0, 100) var step = 1
 export(int) var inital_value = 0
+export(bool) var as_percentage = false
 
 func _ready():
 	$slider.connect("value_changed", self, "display_value")
@@ -26,7 +27,10 @@ func _ready():
 	display_value($slider.value)
 
 func display_value(value):
-	$value.text = str(value)
+	if as_percentage:
+		$value.text = "%" + str(int(value/(max_value-min_value)*100))
+	else:
+		$value.text = str(value)
 
 # Get the value as a string
 func get_value():
