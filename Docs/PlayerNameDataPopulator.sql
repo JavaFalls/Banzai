fed4954153b3d9710595d5bde7aa931455e633aa
masters-of-paradox <mastersofparadox@gmail.com>
2019-04-09 16:34:12 -0500

    new names for database

--- Docs/PlayerNameDataPopulator.sql
+++ Docs/PlayerNameDataPopulator.sql
@@ -78,3 +78,45 @@ INSERT INTO SEI_JavaFalls.javafalls.player_name
              SELECT 3, '{lose();}'
             )
 GO
+
+INSERT INTO SEI_JavaFalls.javafalls.player_name
+            (section, name)
+            (
+             -- Section 1 (front adjectives)
+             --        '123456789'
+             SELECT 1, 'Dr.' UNION ALL
+             SELECT 1, 'Tenacious' UNION ALL
+             SELECT 1, 'Blessed' UNION ALL
+             SELECT 1, 'Speedy' UNION ALL
+             SELECT 1, 'Warrior' UNION ALL
+             SELECT 1, 'Guardian' UNION ALL
+             SELECT 1, 'Fantastic' UNION ALL
+             SELECT 1, 'The' UNION ALL
+             SELECT 1, 'Zealous' UNION ALL
+             SELECT 1, 'Sir' UNION ALL
+             -- Section 2 (names)
+             --        '123456789'
+             SELECT 2, 'Zilch' UNION ALL
+             SELECT 2, 'Accura' UNION ALL
+             SELECT 2, 'Midnight' UNION ALL
+             SELECT 2, 'Sloth' UNION ALL
+             SELECT 2, 'Aristotle' UNION ALL
+             SELECT 2, 'Machine' UNION ALL
+             SELECT 2, 'Campanile' UNION ALL
+             SELECT 2, 'Doomsday' UNION ALL
+             SELECT 2, 'Cookie' UNION ALL
+             SELECT 2, 'Wall' UNION ALL
+             -- Section 3 (back adjectives)
+             --        '123456789'
+             SELECT 3, 'the Bold' UNION ALL
+             SELECT 3, 'the Beast' UNION ALL
+             SELECT 3, 'the Chill' UNION ALL
+             SELECT 3, 'the Agile' UNION ALL
+             SELECT 3, '9000' UNION ALL
+             SELECT 3, 'the Great' UNION ALL
+             SELECT 3, 'Automaton' UNION ALL
+             SELECT 3, 'Master' UNION ALL
+             SELECT 3, 'LittleFox' UNION ALL
+             SELECT 3, ' '
+            )
+GO
